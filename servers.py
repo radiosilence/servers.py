601f62c4bf23fad3c93767051fe0eafb5bd1f373
James Cleveland <jamescleveland@gmail.com>
2011-02-15 19:18:04 +0000

    Made it gunicorn friendly. Goodbye req/sec

--- servers.py
+++ servers.py
@@ -1,23 +1,22 @@
 #!/usr/bin/python
 
+from string import atoi
 import os, sys, stat
 import ConfigParser, os
 import pprint
 
 servers = []
+skels = {}
 
-skels = {
-    'nginx_php': open('skeletons/nginx_php.skel', 'r').readlines(),
-    'nginx_flask': open('skeletons/nginx_flask.skel', 'r').readlines(),
-    'nginx_flat': open('skeletons/nginx_flat.skel', 'r').readlines(),
-    'nginx_php_test': open('skeletons/nginx_php_test.skel', 'r').readlines(),
-    'init_bjoern': open('skeletons/init_bjoern.skel', 'r').readlines()
-}
+for skel in ['nginx_php', 'nginx_flat', 'nginx_php_test', 'nginx_wsgi', 'init_gunicorn']:
+    skels[skel] = open('skeletons/%s.skel' % skel, 'r').readlines()
 
 def main():
     config = ConfigParser.SafeConfigParser()
     config.read('servers.cfg')
     paths = config.defaults()
+    wsgi_name = paths['wsgi_server']
+    port = atoi(paths['port_start'])
 
     for section in config.sections():
         server = {
@@ -36,13 +35,14 @@ def main():
         paths[k] = os.path.abspath(v)
 
     for server in servers:
-        if server['type'] in ['flask']:
+        if server['type'] in ['wsgi']:
+            server['port'] = port
+            port += 1
             server['path'] = paths['apps_dir']
-            
-            init_name = "bjoern_%s" % server['name']
+            init_name = "%s_%s" % (wsgi_name, server['name'])
             server['daemon'] = paths['wsgi_server']
             with open(paths['init_dir'] % init_name, 'w') as f:
-                f.write("".join(skels['init_bjoern']) % server)
+                f.write("".join(skels['init_%s' % wsgi_name]) % server)
                 print "Written init script for %s." % init_name
             f.closed
             os.chmod(paths['init_dir'] % init_name, stat.S_IRWXU)
