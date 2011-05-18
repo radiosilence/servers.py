8f027c1ed0ab0fabcdd30c742327adae0f526bf3
James Cleveland <jamescleveland@gmail.com>
2011-05-18 05:34:25 +0100

    iuno

--- servers.py
+++ servers.py
@@ -8,21 +8,20 @@ import pprint
 servers = []
 skels = {}
 
-for skel in ['nginx_php5', 'nginx_flat', 'nginx_php5_test', 'nginx_wsgi', 'init_gunicorn', 'nginx_php5_ssl']:
+for skel in ['nginx_php5', 'nginx_flat', 'nginx_php5_test', 'nginx_wsgi', 'nginx_php5_ssl', 'supervisor_gunicorn']:
     skels[skel] = open('skeletons/%s.skel' % skel, 'r').readlines()
 
 def main():
     config = ConfigParser.SafeConfigParser()
     config.read('servers.cfg')
     paths = config.defaults()
-    wsgi_name = paths['wsgi_server']
 
     for section in config.sections():
         server = {
             'name': section,
-            'type': config.get(section, 'type')
+            'type': config.get(section, 'type'),
         }
-        for p in ['aliases', 'primary']:
+        for p in ['aliases', 'primary', 'app']:
             try:
                 server[p] = config.get(section, p)
             except ConfigParser.NoOptionError:
@@ -36,13 +35,10 @@ def main():
     for server in servers:
         if server['type'] in ['wsgi']:
             server['path'] = paths['apps_dir']
-            init_name = "%s_%s" % (wsgi_name, server['name'])
-            server['daemon'] = paths['wsgi_server']
-            with open(paths['init_dir'] % init_name, 'w') as f:
-                f.write("".join(skels['init_%s' % wsgi_name]) % server)
-                print "Written init script for %s." % init_name
+            with open(paths['supervisor_conf_dir'] % server['name'] + '.conf', 'w') as f:
+                f.write("".join(skels['supervisor_gunicorn']) % server)
+                print "Written supervisor config for %s." % server['name']
             f.closed
-            os.chmod(paths['init_dir'] % init_name, stat.S_IRWXU)
 
         else:
             server['path'] = paths['vhosts_dir']
